ae8356804483be33b46c8a27c373cc3b4aca7c90
Edgar Oroz <eoroz@indconsulting.com>
2022-10-07 15:40:12 -0700

    Revert "UserControl for Customers"
    This reverts commit e6cfc6bef319fe4974502661ffc4d29140b9cc6b.

--- A03 WiredBrainCoffe.CustomerApp/MainWindow.xaml.cs
+++ A03 WiredBrainCoffe.CustomerApp/MainWindow.xaml.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace A03_WiredBrainCoffe.CustomerApp
 {
@@ -10,6 +23,15 @@ namespace A03_WiredBrainCoffe.CustomerApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            var newColumn = column == 0 
+                ? 2 
+                : 0;
+            CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
         }
     }
 }
