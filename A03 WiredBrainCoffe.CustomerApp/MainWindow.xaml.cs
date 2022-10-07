e6cfc6bef319fe4974502661ffc4d29140b9cc6b
Edgar Oroz <eoroz@indconsulting.com>
2022-10-07 12:36:37 -0700

    UserControl for Customers Place in a new folder call Views

--- A03 WiredBrainCoffe.CustomerApp/MainWindow.xaml.cs
+++ A03 WiredBrainCoffe.CustomerApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace A03_WiredBrainCoffe.CustomerApp
 {
@@ -23,15 +10,6 @@ namespace A03_WiredBrainCoffe.CustomerApp
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
-        {
-            var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
-            var newColumn = column == 0 
-                ? 2 
-                : 0;
-            CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
         }
     }
 }
